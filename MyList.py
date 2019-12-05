7ae7c1bd3afe9678ee77a7294d5461851775ff16
CobaltGoldCS <54545769+CobaltGoldCS@users.noreply.github.com>
2019-12-05 11:22:27 -0700

    Add files via upload

---
+++ MyList.py
@@ -0,0 +1,34 @@
+
+def reverseWords(word):
+    rev = ""
+    for i in range(1, len(word)):
+        rev = rev + word[-i]
+    rev = rev + word[0]
+    return rev
+"""
+Dylan Green
+Reverse
+12/5/19
+"""
+def reverse(target):
+    rev = []
+    for i in range(1, len(target)+1):
+        rev.append(target[-i])
+    return rev
+
+
+
+"""
+Dylan Green
+Sum
+12/5/19
+"""
+
+def sumOfList(targ):
+    length = len(targ)-1
+    new = 0
+    new = new + targ()
+        
+    return value
+        
+print(sumOfList([8,4]))
